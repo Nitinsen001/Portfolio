0ececff2964b9e634d206919e67d96df065e30f1
Nitin Sen <nitinsen91650@gmail.com>
2026-02-21 15:02:59 +0530

    fix: add buildCommand to vercel.json and clean WhiteNoise config for images

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'myproject', 'static'),
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# WhiteNoise settings - serve staticfiles in production
+WHITENOISE_AUTOREFRESH = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
