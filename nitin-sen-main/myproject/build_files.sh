a956477c52419e126f34bce8b0498b96b080e568
jatinmagriyaYT <jatinmagriya4005@gmail.com>
2026-07-01 15:26:23 +0530

    fix: build_files.sh pip install path

--- nitin-sen-main/myproject/build_files.sh
+++ nitin-sen-main/myproject/build_files.sh
@@ -1,2 +1,5 @@
-pip install -r requirements.txt
+#!/bin/bash
+pip install --upgrade pip
+pip install -r requirements.txt --target /tmp/packages
+export PYTHONPATH=/tmp/packages:$PYTHONPATH
 python manage.py collectstatic --noinput
